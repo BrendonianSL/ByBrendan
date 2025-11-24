c337fac34a317ecf161d75cebb21bde6546fe015
Brendan <brendoniansl@gmail.com>
2025-11-23 21:36:44 -0800

    Finished Feature Card Generation.

--- app/_blocks/BlogContainer.tsx
+++ app/_blocks/BlogContainer.tsx
@@ -2,7 +2,7 @@ import BlogCard from "@/pageBlocks/BlogCard"
 
 export default function BlogContainer() {
     return (
-        <section className='grid grid-cols-4 gap-4'>q
+        <section className='grid grid-cols-4 gap-4'>
             <BlogCard />
             <BlogCard />
             <BlogCard />
--- app/_blocks/Featured.tsx
+++ app/_blocks/Featured.tsx
@@ -1,21 +1,28 @@
-import { motion } from 'motion/react';
 import FeaturedCard from './FeaturedCard';
+import { PositionType } from '../_definitions/PositionType';
 
 const fakeArticles = [
     {
         title: 'Something',
         subtitle: 'Something Else',
-    }
-]
+    },
+        {
+        title: 'Something2',
+        subtitle: 'Something Else',
+    },
+        {
+        title: 'Something3',
+        subtitle: 'Something Else',
+    },
+];
+
 export default function Featured() {
     return (
-        <>
+        <div className='flex flex-col gap-8'>
             <h2>Recent Articles</h2>
-            <article className='flex gap-2 flex-col'>
-                {fakeArticles.map((article, index) => (
-                    <FeaturedCard key={index} {...article}/>
-                ))}
+            <article className='flex gap-1 flex-col'>
+                {fakeArticles.map((article, index) => <FeaturedCard key={index} position={index === 0 ? 'top' : index === fakeArticles.length - 1 ? 'bottom' : 'middle'} {...article} articleLength={fakeArticles.length} />)}
             </article>
-        </>
+        </div>
     )
 }
